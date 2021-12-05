9e1e2cfe45b65dc32554edf08812a13d9cb9772a
Flora de la Cruz <91455093+Gingasi@users.noreply.github.com>
2021-12-05 21:57:58 +0100

    FINAL

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,27 +5,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameObject projectilePrefab;
-    
+    private Quaternion Up = Quaternion.Euler(0, 0, 0);
+    private Quaternion Down = Quaternion.Euler(0, 0, 180);
+    private Quaternion Right = Quaternion.Euler(0, 0, -90);
+    private Quaternion Left = Quaternion.Euler(0, 0, 90);
+
     // Update is called once per frame
     void Update()
     {
         // Con esto indicamos hacia que dirección ha de rotar el proyectil cuando salga
+        
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-               SpawnProyectil(Quaternion.Euler(0, 0, 0));
+            SpawnProyectil(Up);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            SpawnProyectil(Quaternion.Euler(0, 0, 180));
+            SpawnProyectil(Down);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            SpawnProyectil(Quaternion.Euler(0, 0, -90));
+            SpawnProyectil(Right);
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            SpawnProyectil(Quaternion.Euler(0, 0, 90));
+            SpawnProyectil(Left);
         }
+      
     }
     public void SpawnProyectil(Quaternion rotate)
     {
